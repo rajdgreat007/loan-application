fec117ed9f772b66070eaebd43dd784870d63b5e
Raj Gupta <raj.gupta@walmartlabs.com>
2019-09-15 20:48:30 +0530

    Updates

---
+++ precache-manifest.19d39ddd99340288d275d6123a25366c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea2542aa80188418f869295f81b6f58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ed05e071f81caab244d",
+    "url": "/static/css/main.51536958.chunk.css"
+  },
+  {
+    "revision": "28a9dbb3ac767f65b5f6",
+    "url": "/static/js/2.9205f6ef.chunk.js"
+  },
+  {
+    "revision": "6ed05e071f81caab244d",
+    "url": "/static/js/main.67d2c19e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
